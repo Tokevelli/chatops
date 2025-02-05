71cd88738a6f6f1cd7b47a079fdea4b35b7f4dc5
joseph <tokevelli@rocketmail.com>
2025-02-05 02:17:10 +0000

    updates to like feature

--- project/models.py
+++ project/models.py
@@ -24,3 +24,12 @@ class User(db.Model):
 
     def __str__(self):
         return f"Username: {self.name}"
+
+class Like(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    __table_args__ = (db.UniqueConstraint('post_id', 'user_id', name='unique_like'),)
+
+    def __repr__(self):
+        return f'<Like post_id={self.post_id} user_id={self.user_id}>'
--- project/static/main.js
+++ project/static/main.js
@@ -5,7 +5,17 @@
 const postElements = document.getElementsByClassName("entry");
 
 for (var i = 0; i < postElements.length; i++) {
-  postElements[i].addEventListener("click", function () {
+  postElements[i].addEventListener("click", function (event) {
+    // Prevents the deletion if a button inside the post is clicked
+    if (event.target.tagName.toLowerCase() === 'button') {
+      // Call handleLike if a button is clicked and it's a like button
+      if (event.target.classList.contains('like-button')) {
+        const postId = event.target.dataset.postId;
+        handleLike(postId);
+      }
+      return; // Stop further event processing
+    }
+    
     const postId = this.getElementsByTagName("h2")[0].getAttribute("id");
     const node = this;
     fetch(`/delete/${postId}`)
@@ -23,4 +33,19 @@ for (var i = 0; i < postElements.length; i++) {
         console.log(err);
       });
   });
+}
+
+// Function to handle likes
+function handleLike(postId) {
+  fetch(`/like_post/${postId}`, { method: 'POST' })
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'liked' || data.status === 'unliked') {
+        const likesCountElement = document.getElementById(`like_count_${postId}`);
+        likesCountElement.innerText = `${data.likes_count} Likes`; // Update like count from server response
+      } else {
+        alert(data.message); // Alert the message from the server if any
+      }
+    })
+    .catch(error => console.error('Error liking the post:', error));
 }

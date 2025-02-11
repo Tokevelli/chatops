b5c010f776697dbe48c99c2b3cfbc14bcf8e98d1
DevlinO <150296932+DBOSteezy@users.noreply.github.com>
2025-02-11 12:03:53 -0700

    Update create_db.py
    Manual update of create_db.py when push not working

--- create_db.py
+++ create_db.py
@@ -5,6 +5,9 @@ from project.app import app, db
 
 
 with app.app_context():
+    # Drop all existing tables and ensure a clean slate
+    db.drop_all()
+    
     # create the database and the db table
     db.create_all()
 
